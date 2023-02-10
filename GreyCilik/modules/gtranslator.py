b179faf5a02e5cbeca713fb0c77e26295ed141f9
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-02-11 05:50:26 +0700

    Update gtranslator.py

--- GreyCilik/modules/gtranslator.py
+++ GreyCilik/modules/gtranslator.py
@@ -15,24 +15,29 @@ from GreyCilik.modules.disable import DisableAbleCommandHandler
 
 
 __help__ = """ 
-Use this module to translate stuff!
-*Commands:*
-✪ /tl (or /tr): as a reply to a message, translates it to English.
-✪ /tl <lang>: translates to <lang>
-eg: /tl ja: translates to Japanese.
-✪ /tl <source>//<dest>: translates from <source> to <lang>.
-eg:  /tl ja//en: translates from Japanese to English.
-✪ /langs: get a list of supported languages for translation.
-I can convert text to voice and voice to text..
-✪ /tts <lang code>*:* Reply to any message to get text to speech output
-✪ /stt*:* Type in reply to a voice message(support english only) to extract text from it.
-*Language Codes*
-`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
-et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
-id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
-ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
-sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
-vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
+──「 Penerjemah 」──
+
+Gunakan modul ini untuk menerjemahkan barang!
+
+*Perintah:*
+❖ /tl (atau /tr): sebagai balasan pesan, menerjemahkannya ke bahasa Inggris.
+❖ /tl <lang>: diterjemahkan menjadi <lang>
+   misalnya: /tl ja: diterjemahkan ke bahasa Jepang.
+❖ /tl <source>//<dest>: menerjemahkan dari <source> ke <lang>.
+   misalnya: /tl ja//en: menerjemahkan dari bahasa Jepang ke bahasa Inggris.
+❖ /langs: dapatkan daftar bahasa yang didukung untuk terjemahan.
+
+Saya dapat mengubah teks menjadi suara dan suara menjadi teks..
+❖ /tts <lang code>: Membalas pesan apa pun untuk mendapatkan keluaran teks ke ucapan
+❖ /stt: Ketikkan balasan ke pesan suara (hanya mendukung bahasa Inggris) untuk mengekstrak teks darinya.
+
+*Kode Bahasa*
+  `af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+  et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hai,hmn,hr,ht,hu,hy,
+  id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+  ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+  sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+  vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
 
 __mod_name__ = "Translator"
